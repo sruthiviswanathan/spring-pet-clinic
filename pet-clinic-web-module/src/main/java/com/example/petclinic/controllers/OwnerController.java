e48f3a6be37f2ff84b95d0b85c6eb31da05b7de8
sruthiviswanathan <sravan1097@gmail.com>
2021-01-15 17:42:24 +0530

    Listed all owners in owners page. Closes #16.

--- pet-clinic-web-module/src/main/java/com/example/petclinic/controllers/OwnerController.java
+++ pet-clinic-web-module/src/main/java/com/example/petclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.example.petclinic.controllers;
 
+import com.example.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    @Autowired
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/" ,"/index", "/index.html"})
-    public String ownerList() {
+    public String ownerList(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
