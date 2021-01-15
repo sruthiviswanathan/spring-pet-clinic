6522f8ddc5a507faf83e3d374efe04112cfa6156
sruthiviswanathan <sravan1097@gmail.com>
2021-01-15 10:21:33 +0530

    Added common base interface. Closes #11.

--- pet-clinic-data-module/src/main/java/com/example/petclinic/services/PetService.java
+++ pet-clinic-data-module/src/main/java/com/example/petclinic/services/PetService.java
@@ -3,8 +3,5 @@ package com.example.petclinic.services;
 import com.example.petclinic.model.Pet;
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data-module/src/main/java/com/example/petclinic/services/VetService.java
+++ pet-clinic-data-module/src/main/java/com/example/petclinic/services/VetService.java
@@ -3,8 +3,5 @@ package com.example.petclinic.services;
 import com.example.petclinic.model.Vet;
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
