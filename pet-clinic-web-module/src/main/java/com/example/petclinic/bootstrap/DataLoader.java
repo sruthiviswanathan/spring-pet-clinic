244fd46a996c9364dbb261194b3242c06512f55c
sruthiviswanathan <sravan1097@gmail.com>
2021-01-15 17:52:28 +0530

    Listed all vets in vet page. Closes #15.

--- pet-clinic-web-module/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web-module/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -50,7 +50,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(1L);
+        vet2.setId(2L);
         vet2.setFirstName("Maxi");
         vet2.setLastName("Nick");
         vetService.save(vet2);
