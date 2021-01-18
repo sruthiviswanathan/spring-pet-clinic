ee2d1077259ec138a2a251be382ea57f2869abce
sruthiviswanathan <sravan1097@gmail.com>
2021-01-18 17:32:51 +0530

    Loaded pet Types on application startup. Closes #31.

--- pet-clinic-web-module/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web-module/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -1,33 +1,56 @@
 package com.example.petclinic.bootstrap;
 
 import com.example.petclinic.model.Owner;
+import com.example.petclinic.model.Pet;
+import com.example.petclinic.model.PetType;
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.services.OwnerService;
+import com.example.petclinic.services.PetService;
+import com.example.petclinic.services.PetTypeService;
 import com.example.petclinic.services.VetService;
-import com.example.petclinic.services.map.OwnerServiceMap;
-import com.example.petclinic.services.map.VetServiceMap;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
+    private final PetService petService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, PetService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+        this.petService = petService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded petTypes...");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
         ownerService.save(owner1);
+
+//        Pet pet1 = new Pet();
+//        pet1.setPetType(savedDogPetType);
+//        pet1.setBirthDate(LocalDate.ofYearDay(2020, 5));
+//        pet1.setOwner(owner1);
+//        petService.save(pet1);
+//        System.out.println("Saved Pet...");
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Micheal");
