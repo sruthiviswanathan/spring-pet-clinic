52ba40a3f1a30f3173ce8a494e2557be5bce8fed
sruthiviswanathan <sravan1097@gmail.com>
2021-01-18 14:20:05 +0530

    Implemented WRO4J maven plugin and SPC resources. Closes #23.

--- pet-clinic-web-module/src/main/java/com/example/petclinic/controllers/IndexController.java
+++ pet-clinic-web-module/src/main/java/com/example/petclinic/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.example.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("welcomeMessage", "Welcome to pet clinic");
         return "index";
     }
 }
