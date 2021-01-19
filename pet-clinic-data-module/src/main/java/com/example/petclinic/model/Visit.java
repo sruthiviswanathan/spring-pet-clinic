ec95cd3c1337fce0cb139e7e63cc4d73203cca9a
sruthiviswanathan <sravan1097@gmail.com>
2021-01-19 19:00:26 +0530

    Converted owner to JPA entity. Closes #35.

--- pet-clinic-data-module/src/main/java/com/example/petclinic/model/Visit.java
+++ pet-clinic-data-module/src/main/java/com/example/petclinic/model/Visit.java
@@ -1,7 +1,9 @@
 package com.example.petclinic.model;
 
+import javax.persistence.Entity;
 import java.time.LocalDate;
 
+@Entity
 public class Visit extends BaseEntity {
 
     private LocalDate date;
