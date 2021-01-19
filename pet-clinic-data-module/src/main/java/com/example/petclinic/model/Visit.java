c02c81417932306fa2306d03ce6ba555f524698c
sruthiviswanathan <sravan1097@gmail.com>
2021-01-19 19:15:52 +0530

    Converted Visit to JPA entity. Closes #37.

--- pet-clinic-data-module/src/main/java/com/example/petclinic/model/Visit.java
+++ pet-clinic-data-module/src/main/java/com/example/petclinic/model/Visit.java
@@ -1,13 +1,18 @@
 package com.example.petclinic.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
